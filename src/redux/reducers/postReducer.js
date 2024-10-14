f14a6829833b1c6d2038f2cdbee1b44477912367
kasia.kordys <kasia.kordys@gmail.com>
2024-10-14 19:40:38 +0300

    add actions to get a single post

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -12,18 +12,19 @@ const initialState = {
 
 // actions
 
+// FETCH ALL POSTS
 // start fetch request
 
 const fetchPostsRequest = () => {
   return {
-    type: "FETCH_STARTED",
+    type: "FETCH_POSTS_STARTED",
   };
 };
 
 // fetch successful
 const fetchPostsSuccess = (posts) => {
   return {
-    type: "FETCH_SUCCESS",
+    type: "FETCH_POSTS_SUCCESS",
     payload: posts,
   };
 };
@@ -31,7 +32,7 @@ const fetchPostsSuccess = (posts) => {
 // fetch failed
 const fetchPostsFailed = (error) => {
   return {
-    type: "FETCH_FAILED",
+    type: "FETCH_POSTS_FAILED",
     payload: error,
   };
 };
@@ -45,9 +46,50 @@ const fetchPostsAction = () => {
       //get data from the api
       const response = await axios.get(apiURL);
       // dispatch success
-      dispatch(fetchPostsSuccess(response));
+      dispatch(fetchPostsSuccess(response.data));
     } catch (error) {
       dispatch(fetchPostsFailed(error));
+    }
+  };
+};
+
+// FETCH A SINGLE POST
+// start fetch request
+
+const fetchPostRequest = () => {
+  return {
+    type: "FETCH_POST_STARTED",
+  };
+};
+
+// fetch successful
+const fetchPostSuccess = (post) => {
+  return {
+    type: "FETCH_POST_SUCCESS",
+    payload: post,
+  };
+};
+
+// fetch failed
+const fetchPostFailed = (error) => {
+  return {
+    type: "FETCH_POSTS_FAILED",
+    payload: error,
+  };
+};
+
+// dispatch request/async
+const fetchPostAction = (id) => {
+  return async (dispatch) => {
+    // dispatch fetch
+    dispatch(fetchPostRequest());
+    try {
+      //get data from the api
+      const response = await axios.get(`apiURL/${id}`);
+      // dispatch success
+      dispatch(fetchPostSuccess(response.data));
+    } catch (error) {
+      dispatch(fetchPostFailed(error));
     }
   };
 };
