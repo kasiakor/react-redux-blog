b398f4444f2de4d31334b25d138a84cea96fc8f1
kasia.kordys <kasia.kordys@gmail.com>
2024-10-14 19:30:57 +0300

    add actions to get all posts

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import apiURL from "../../utils/apiURL";
+
 // initial state
 
 const initialState = {
@@ -5,4 +8,46 @@ const initialState = {
   posts: [],
   post: {},
   error: "",
+};
+
+// actions
+
+// start fetch request
+
+const fetchPostsRequest = () => {
+  return {
+    type: "FETCH_STARTED",
+  };
+};
+
+// fetch successful
+const fetchPostsSuccess = (posts) => {
+  return {
+    type: "FETCH_SUCCESS",
+    payload: posts,
+  };
+};
+
+// fetch failed
+const fetchPostsFailed = (error) => {
+  return {
+    type: "FETCH_FAILED",
+    payload: error,
+  };
+};
+
+// dispatch request/async
+const fetchPostsAction = () => {
+  return async (dispatch) => {
+    // dispatch fetch
+    dispatch(fetchPostsRequest());
+    try {
+      //get data from the api
+      const response = await axios.get(apiURL);
+      // dispatch success
+      dispatch(fetchPostsSuccess(response));
+    } catch (error) {
+      dispatch(fetchPostsFailed(error));
+    }
+  };
 };
