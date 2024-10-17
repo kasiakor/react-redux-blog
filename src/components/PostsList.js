4108161f9533ae4b28c8564fe60c402909e42f67
kasia.kordys <kasia.kordys@gmail.com>
2024-10-17 14:32:23 +0300

    fetch and display all posts

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,17 +1,33 @@
 import React, { useEffect } from "react";
-import SearchPost from "./SearchPost";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPostsAction } from "../redux/actions/postActions";
 import "./Posts.css";
+import SearchPost from "./SearchPost";
 
 const PostsList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPostsAction());
+    // eslint-disable-next-line
+  }, []);
+
+  const { loading, error, posts, post } = useSelector((data) => data);
+
+  console.log("data", loading, error, posts, post);
   return (
     <>
       <SearchPost />
       <div className="posts-list">
-        <h1>Total Posts 100</h1>
-        <div className="post-details">
-          <h3>Post Title 1</h3>
-          <p>Post body 1</p>
-        </div>
+        <h1>Total Posts {posts.length}</h1>
+        {loading && <h2>Loading...</h2>}
+        {error && <h2>Something went wrong {error}</h2>}
+        {posts.map((post) => (
+          <div className="post-details">
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     </>
   );
